43df16485c3960e58dd41f72eae5f6982d3618fd
goomon <jaegoo.jho@gmail.com>
2023-02-21 16:44:27 +0900

    Refac: topic structure
    * sensor data based topic -> one topic structure

---
+++ locustfile.py
@@ -0,0 +1,29 @@
+import json
+import time
+import uuid
+
+from locust import task, User
+
+from config.constants import MAIN_TOPIC
+from kafka_client import KafkaClient
+from mock_generator import MockSensorDataGenerator
+
+
+class KafkaLocust(User):
+    mock_generator = MockSensorDataGenerator
+    client = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_id = None
+        KafkaLocust.client = KafkaClient()
+
+    def on_start(self):
+        self.user_id = str(uuid.uuid4())
+        return super().on_start()
+
+    @task
+    def send_data(self):
+        msg = KafkaLocust.mock_generator.generate_data(self.user_id)
+        self.client.send(MAIN_TOPIC, key=self.user_id, message=json.dumps(msg))
+        time.sleep(0.5)
---
+++ mock_generator.py
@@ -0,0 +1,46 @@
+import random
+import time
+from collections import defaultdict
+
+from config.constants import *
+from schema.schema import SensorRecord
+
+
+class MockSensorDataGenerator:
+    timestamp_map: dict[str, float] = defaultdict(time.time)
+
+    @staticmethod
+    def generate_data(user_id: str) -> SensorRecord:
+        MockSensorDataGenerator.timestamp_map[user_id] += 1
+        timestamp = MockSensorDataGenerator.timestamp_map[user_id] + 1
+        template: SensorRecord = {
+            "user_id": user_id,
+            "timestamp": int(timestamp * 1000),
+            "value": {
+                "acc": {
+                    "hz": ACC_HZ,
+                    "value": [{
+                        "x": random.uniform(-20, 20),
+                        "y": random.uniform(-20, 20),
+                        "z": random.uniform(-20, 20)
+                    } for _ in range(ACC_HZ)]
+                },
+                "bvp": {
+                    "hz": BVP_HZ,
+                    "value": [random.uniform(-400, 1200) for _ in range(BVP_HZ)]
+                },
+                "eda": {
+                    "hz": EDA_HZ,
+                    "value": [random.uniform(0.3, 1.5) for _ in range(EDA_HZ)]
+                },
+                "hr": {
+                    "hz": HR_HZ,
+                    "value": [random.uniform(80, 10) for _ in range(HR_HZ)]
+                },
+                "temp": {
+                    "hz": TEMP_HZ,
+                    "value": [random.gauss(36.5, 1) for _ in range(TEMP_HZ)]
+                }
+            }
+        }
+        return template
